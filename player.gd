ffc986e2d8e699275096b9353e1284a89899defd
Tylefrost <114878022+Tylefrost@users.noreply.github.com>
2024-08-18 20:06:13 -0700

    ok so jumping added and no collide w/ handholds

--- player.gd
+++ player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody2D
 
-var speed = 300
-var gravity_strength = 50
+#affects swing and jump
+@export var gravity_strength = 50
 
+#jumping
+@export var max_jump_force = 1000
+@export var min_jump_force = 100
+@export var jump_charge_time = 1
+var is_charging_jump = false
+var charge_start_time = 0
+
+#swinging
 var hook_pos = Vector2()
 var hooked = false
 var rope_length = 10
 var current_rope_length
+
+var speed = 300
 
 func _ready():
 	current_rope_length = rope_length
@@ -35,6 +45,23 @@ func _physics_process(delta):
 	move(delta)
 	move_and_slide()
 	
+	if Input.is_action_just_pressed("jump"):
+		print("jump")
+		is_charging_jump = true
+		charge_start_time = Time.get_ticks_msec() / 1000.0
+		
+	elif Input.is_action_just_released("jump") and is_charging_jump:
+		print("jump2")
+		is_charging_jump = false
+		velocity.y = -get_jump_force()
+		
+	move_and_slide()
+	
+func get_jump_force() -> float:
+	var charge_duration = clamp(Time.get_ticks_msec() / 1000.0 - charge_start_time, 0, jump_charge_time)
+	var jump_force = min_jump_force + (max_jump_force - min_jump_force) * (charge_duration / jump_charge_time)
+	return jump_force
+	
 func _draw():
 	var pos = global_position
 	
@@ -42,8 +69,6 @@ func _draw():
 		draw_line(Vector2(0, -16), to_local(hook_pos), Color(0.35, 0.7, 0.9), 3, true)
 	else:
 		return
-		
-		
 	
 func hook():
 	$Raycast.look_at(get_global_mouse_position())
@@ -55,6 +80,7 @@ func hook():
 	if Input.is_action_just_released("left_click") and hooked:
 		velocity.y -= 15 * gravity_strength
 		hooked = false
+		
 func get_hook_pos():
 	for raycast in $Raycast.get_children():
 		if raycast.is_colliding():
