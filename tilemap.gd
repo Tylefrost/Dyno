f7f8edd3f69deb7cf9d887bf0312b3c6c55ff2f0
Richmond Yang <richmond.yang@gmail.com>
2024-08-18 18:29:06 -0700

    Procedural Level Generation

---
+++ tilemap.gd
@@ -0,0 +1,13 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	
+	for cell_position in get_used_cells():
+		var row = rng.randi_range(0,2)
+		var column = rng.randi_range(0,2)
+		set_cell(cell_position, 0, Vector2i(row, column))
+		print(cell_position, row, column)
